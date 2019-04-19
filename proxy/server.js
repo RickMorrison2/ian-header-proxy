7f04a770a60024efedca4a1c0c91210fa4ee1046
Ian <isalmon1@gmail.com>
2019-04-19 12:12:49 -0600

    Added createProxyServer to render images from AWS public DNS constainer instances hosted through ECS.

--- proxy/server.js
+++ proxy/server.js
@@ -2,10 +2,59 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 3000;
+const httpProxy = require('http-proxy');
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+const proxy = httpProxy.createProxyServer({});
+
+// Ian's route
+app.all("/data/artist/*", (req, res) => {
+  proxy.web(req, res, {
+    target: "http://ec2-18-191-230-44.us-east-2.compute.amazonaws.com/"
+  });
+});
+
+// Dan's route
+app.all("/data/toptracks/", (req, res) => {
+  proxy.web(req, res, {
+    target: "http://ec2-18-191-178-115.us-east-2.compute.amazonaws.com/"
+  });
+});
+
+// Bevan's routes
+app.get('/data/albumswithartist/*', (req, res) => {
+  proxy.web(req, res, {
+    target: 'http://ec2-3-17-177-153.us-east-2.compute.amazonaws.com/'
+  });
+});
+app.get('/data/albumsbyartist/*', (req, res) => {
+  proxy.web(req, res, {
+    target: 'http://ec2-3-17-177-153.us-east-2.compute.amazonaws.com/'
+  });
+});
+app.get('/data/epswithartist/*', (req, res) => {
+  proxy.web(req, res, {
+    target: 'http://ec2-3-17-177-153.us-east-2.compute.amazonaws.com/'
+  });
+});
+app.get('/data/compilationswithartist/*', (req, res) => {
+  proxy.web(req, res, {
+    target: 'http://ec2-3-17-177-153.us-east-2.compute.amazonaws.com/'
+  });
+});
+
+// Dmitri's route
+app.all("/data/artist/", (req, res) => {
+  proxy.web(req, res, {
+    target: "http://ec2-34-227-148-64.compute-1.amazonaws.com/"
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
